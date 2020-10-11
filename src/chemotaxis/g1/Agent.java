07ac132d9a8b434a059329c0cbe04ad093be58a6
AumUpadhyay <aumspeaks@gmail.com>
2020-10-11 11:16:03 -0700

    Updated Agent to go towards maximum affinity

--- src/chemotaxis/g1/Agent.java
+++ src/chemotaxis/g1/Agent.java
@@ -1,7 +1,9 @@
 package chemotaxis.g1; // TODO modify the package name to reflect your team
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
@@ -39,14 +41,15 @@ public class Agent extends chemotaxis.sim.Agent {
     public Move makeMove(Integer randomNum, Byte previousState, ChemicalCell currentCell,
                          Map<DirectionType, ChemicalCell> neighborMap) {
         Move agentMove = new Move();
-        Map<DirectionType, ChemicalType> move = getMostConcentratedChemical(neighborMap);
-        for(DirectionType key: move.keySet()){
-            if(move.get(key) == ChemicalType.BLUE){
-                agentMove.directionType = key;
-            } else if (move.get(key) == ChemicalType.RED){
-                agentMove.directionType = getOppositeDirection(key);
-            }
-        }
+        // Map<DirectionType, ChemicalType> move = getMostConcentratedChemical(neighborMap);
+        // for(DirectionType key: move.keySet()){
+        //     if(move.get(key) == ChemicalType.BLUE){
+        //         agentMove.directionType = key;
+        //     } else if (move.get(key) == ChemicalType.RED){
+        //         agentMove.directionType = getOppositeDirection(key);
+        //     }
+        // }
+        agentMove.directionType = getStrongestAffinity(neighborMap);
         return agentMove;
     }
 
@@ -84,4 +87,57 @@ public class Agent extends chemotaxis.sim.Agent {
             return DirectionType.NORTH;
         }
     }
+
+    private Map<DirectionType, List<Double>> getAffinities(Map<DirectionType, ChemicalCell> neighborCellMap){
+        double blueAffinity = 1.0;
+        double redAffinity = -1.0;
+        double greenAffinity = 10.0;
+        Map<DirectionType, List<Double>> affinity = new HashMap<>();
+        for (DirectionType neighborDir : neighborCellMap.keySet()) {
+            ChemicalCell neighbor = neighborCellMap.get(neighborDir);
+            Map<ChemicalType, Double> neighborMap = neighbor.getConcentrations();
+            ArrayList<Double> affinities = new ArrayList<>();
+            for (Entry<ChemicalType, Double> entry : neighborMap.entrySet()) { // Itrate through hashmap
+                if(entry.getKey().equals(ChemicalType.BLUE)){
+                    affinities.add(blueAffinity*entry.getValue());
+                } else if(entry.getKey().equals(ChemicalType.RED)){
+                    affinities.add(redAffinity*entry.getValue());
+                } else {
+                    affinities.add(greenAffinity*entry.getValue());
+                }
+            }
+            affinity.put(neighborDir, affinities);
+        }
+        return affinity;
+    }
+
+    private DirectionType getStrongestAffinity(Map<DirectionType, ChemicalCell> neighborCellMap){
+        Map<DirectionType, List<Double>> affinityMap = getAffinities(neighborCellMap);
+        Map<DirectionType, Double> maxAffinityMap = new HashMap<>();
+        for (DirectionType neighborDir : affinityMap.keySet()) {
+            List<Double> affinities = affinityMap.get(neighborDir);
+            int maxIndex = 0;
+            for (int i = 0; i < affinities.size(); i++){
+                if(Math.abs(affinities.get(i)) > affinities.get(maxIndex)){
+                    maxIndex = i;
+                }
+            }
+            maxAffinityMap.put(neighborDir, affinities.get(maxIndex));
+        }
+        //TO-DO Add default behavior
+        DirectionType max = DirectionType.NORTH;
+        double maxAffinity = -1.0;
+        for(DirectionType dir : maxAffinityMap.keySet()){
+            if(Math.abs(maxAffinityMap.get(dir)) > maxAffinity){
+                maxAffinity = maxAffinityMap.get(dir);
+                max = dir;
+            }
+        }
+        if(maxAffinity < 0.0){
+            return getOppositeDirection(max);
+        } else {
+            return max;
+        }
+    }
+
 }
