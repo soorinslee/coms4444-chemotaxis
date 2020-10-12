64b7d99237e50136c224e8f5243b8501b374c04c
JosephHier <jdh2190@columbia.edu>
2020-10-12 07:19:37 -0700

    Fix bugs in applying colors.

--- src/chemotaxis/sim/Simulator.java
+++ src/chemotaxis/sim/Simulator.java
@@ -47,14 +47,14 @@ public class Simulator {
 	
 	// Defaults
 	private static boolean validMap = true;
-	private static boolean enableControllerPrints = false;
-	private static boolean enableAgentPrints = false;
+	private static boolean enableControllerPrints = true;//false;
+	private static boolean enableAgentPrints = true; //false;
 	private static int chemicalsRemaining = budget;
 	private static Point start, target, agentLocation;
 	private static List<Point> blockedLocations;
 	private static Map<ChemicalType, Integer> chemicalsUsed;
 	private static int mapSize = 100;
-	private static long timeout = 1000;
+	private static long timeout = Integer.MAX_VALUE;//1000;
 	private static int currentTurn = 0;
 	private static String version = "1.0";
 	private static String projectPath, sourcePath, staticsPath, guiPath;
@@ -62,7 +62,7 @@ public class Simulator {
 
 	private static void setup() {
 		projectPath = new File(".").getAbsolutePath().substring(0, 
-				new File(".").getAbsolutePath().indexOf("coms4444-chemotaxis") + "coms4444-chemotaxis".length());
+				new File(".").getAbsolutePath().indexOf("project-2") + "project-2".length());
 		sourcePath = projectPath + File.separator + "src";
 		staticsPath = projectPath + File.separator + "statics";
 	}
