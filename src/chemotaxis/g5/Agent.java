1ba6d02a6ae6cad8247c1e65a70558e7d5b7b9eb
kattu99 <kataria.rahul172@gmail.com>
2020-10-14 11:20:21 -0400

    Modified Agent to include shortest turn path

--- src/chemotaxis/g5/Agent.java
+++ src/chemotaxis/g5/Agent.java
@@ -1,4 +1,4 @@
-package chemotaxis.g5;
+package chemotaxis.turns;
 
 import java.util.Map;
 
@@ -36,58 +36,91 @@ public class Agent extends chemotaxis.sim.Agent {
 	   // TODO add your code here to move the agent
 	   Move move = new Move();
 	   // States: 
-	   //0: I'm following BLUE
-	   //1: I'm following GREEN
-	   //2: I'm following RED
-
-		
-	   ChemicalType chosenChemicalType;
+	   //0: Wait for chemicals to decide where to go
+	   //1: I'm moving NORTH
+	   //2: I'm moving EAST
+	   //3: I'm moving SOUTH
+	   //4: I'm moving WEST
+	   
 	   if (previousState == 0) {
-		   chosenChemicalType= ChemicalType.BLUE;
-	   } 
-	   else if (previousState == 1) {
-		   chosenChemicalType= ChemicalType.GREEN;
-	   } 
-	   else {
-		   chosenChemicalType= ChemicalType.RED;
-	   }
-	   boolean atHighest = true;
-	   boolean allZero = true;
-	   double highestConcentration = currentCell.getConcentration(chosenChemicalType);
-	   for(DirectionType directionType : neighborMap.keySet()) {
-		   double neighborConcentration = neighborMap.get(directionType).getConcentration(chosenChemicalType);
-		   //System.out.println("for loop, neighborConcentration: "+neighborConcentration);
-		   //System.out.println("for loop, directionType: "+directionType);
-		   if(highestConcentration < neighborConcentration) {
-			   highestConcentration = neighborMap.get(directionType).getConcentration(chosenChemicalType);
-			   move.directionType = directionType;
-			   move.currentState = previousState;
-			   atHighest = false;
+		   move.currentState = 4;
+		   move.directionType = DirectionType.WEST;
+		   for (ChemicalType color: ChemicalType.values()) {
+			   double concentration = currentCell.getConcentration(color);
+			   if (concentration == 1.0) {
+				   if (color == ChemicalType.RED) {
+					   move.currentState = 1;
+					   move.directionType = DirectionType.NORTH;
+				   } else if (color == ChemicalType.GREEN) {
+					   move.currentState = 2;
+					   move.directionType = DirectionType.EAST;
+				   } else if (color == ChemicalType.BLUE) {
+					   move.currentState = 3;
+					   move.directionType = DirectionType.SOUTH;
+				   }
+			   }
 		   }
-		   if (neighborConcentration > 0.01) {
-			   allZero = false;
-		   }
-	   }
-	   if (atHighest) {
-		   move.directionType = DirectionType.CURRENT;
-		   if (!allZero) {
-			   move.currentState = (byte) (previousState + 1);
-			   if (move.currentState >= 3) {
-				   move.currentState = 0;
-			   } 
-		   }
-		   else {
-			   move.currentState = previousState;
+	   } else {
+		   double red = currentCell.getConcentration(ChemicalType.RED);
+		   double green = currentCell.getConcentration(ChemicalType.GREEN);
+		   if (red == 1.0) {
+			   move = turnLeft(move, previousState);
+		   } else if (green == 1.0) {
+			   move = turnRight(move, previousState);
+		   } else {
+			   move = straight(move, previousState);
 		   }
 	   }
 
-	   //System.out.println("Chemical Type: "+chosenChemicalType);
-	   //System.out.println("highestConcentration: "+highestConcentration);
-	   //System.out.println("direction: "+move.directionType);
-	   //System.out.println("PreviousState: "+previousState);
-	   //System.out.println("CurrentState: "+move.currentState);
-		
 	   return move;
 	   // TODO modify the return statement to return your agent move
+   }
+   
+   public Move turnLeft(Move move, Byte previousState) {
+	   if (previousState == 1) {
+		   move.directionType = DirectionType.WEST;
+		   move.currentState = 4;
+	   } else if (previousState == 2) {
+		   move.directionType = DirectionType.NORTH;
+		   move.currentState = 1;
+	   } else if (previousState == 3) {
+		   move.directionType = DirectionType.EAST;
+		   move.currentState = 2;
+	   } else if (previousState == 4) {
+		   move.directionType = DirectionType.SOUTH;
+		   move.currentState = 3;
+	   }
+	   return move;
+   }
+   
+   public Move turnRight(Move move, Byte previousState) {
+	   if (previousState == 1) {
+		   move.directionType = DirectionType.EAST;
+		   move.currentState = 2;
+	   } else if (previousState == 2) {
+		   move.directionType = DirectionType.SOUTH;
+		   move.currentState = 3;
+	   } else if (previousState == 3) {
+		   move.directionType = DirectionType.WEST;
+		   move.currentState = 4;
+	   } else if (previousState == 4) {
+		   move.directionType = DirectionType.NORTH;
+		   move.currentState = 1;
+	   }
+	   return move;
+   }
+   
+   public Move straight(Move move, Byte previousState) {
+	   if (previousState == 1) {
+		   move.directionType = DirectionType.NORTH;
+	   } else if (previousState == 2) {
+		   move.directionType = DirectionType.EAST;
+	   } else if (previousState == 3) {
+		   move.directionType = DirectionType.SOUTH;
+	   } else if (previousState == 4) {
+		   move.directionType = DirectionType.WEST;
+	   }
+	   move.currentState = previousState;
+	   return move;
    }
 }
