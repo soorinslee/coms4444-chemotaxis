df32da5df4e5a7b9c3d854e55fc603abef16e969
Soo Rin Lee <soorinslee@gmail.com>
2020-10-18 18:36:23 -0400

    Check for vertical path

--- src/chemotaxis/g3/PathOptimizer.java
+++ src/chemotaxis/g3/PathOptimizer.java
@@ -10,27 +10,61 @@ public class PathOptimizer {
 
     private static SimPrinter simPrinter = new SimPrinter(true);
 
-    public static List<Point> getPath(List<Point> path) {
+    public static List<Point> getPath(List<Point> path, ChemicalCell[][] grid) {
         List<Point> newPath = new ArrayList<Point>();
         Point turnPt = null;
         Point curPt = path.get(0);
 
-        while (!curPt.equals(path.get(path.size()-1))) {
+        //add start point
+        newPath.add(path.get(0));
 
+        while (!curPt.equals(path.get(path.size()-1))) {
+            newPath.add(turnPt);
+            curPt.setLocation(turnPt);
         }
+
+        //add target point
+        newPath.add(path.get(path.size()-1));
 
         return newPath;
     }
 
-    public static checkVertical(Point curPt, Point futPt) {
+    public static Point checkVertical(Point curPt, Point futPt, ChemicalCell[][] grid) {
+        Point temp = curPt;
+        int addVal = 0;
+
+        if (Double.compare(temp.getY(), futPt.getY()) == 0) 
+        {
+            return new Point(0, 0);
+        }
+	    else {
+            if (Double.compare(temp.getX(), futPt.getX()) > 0) {
+                addVal = 1;
+            }
+            else {
+                addVal = -1;
+            }
+
+            while (!temp.equals(futPt)) {
+                int x = (int) temp.getX();
+                int y = (int) temp.getY();
+                if (grid[x - 1][y - 1].isOpen == false) {
+                    return new Point(0, 0);
+                }
+                else {
+                    temp.setLocation(x + addVal, y);
+                }
+            }
+        }
+
+	    return temp;
+    }
+
+    public static checkHorizontal(Point curPt, Point futPt, ChemicalCell[][] grid) {
 
     }
 
-    public static checkHorizontal(Point curPt, Point futPt) {
+    public static checkDiagonal(Point curPt, Point futPt, ChemicalCell[][] grid) {
 
-    }
-
-    public static checkDiagonal(Point curPt, Point futPt) {
-        
     }
 }
